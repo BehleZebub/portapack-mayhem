54376b757094b5724425169901a520e07bfda150
BehleZebub <116460179+BehleZebub@users.noreply.github.com>
2022-11-07 23:16:31 +0100

    create basic menu

--- firmware/application/apps/ui_apt_rx.cpp
+++ firmware/application/apps/ui_apt_rx.cpp
@@ -7,37 +7,47 @@ using namespace portapack;
 namespace ui
 {
 
-    void APTRXView::focus() {                                // Default selection to button_helloWorld when app starts
-	    button_helloWorld.focus();
+    void APTRXView::focus()
+    {                                // Default selection to button_helloWorld when app starts
+	    options_freq.focus();
     }
 
-    APTRXView::APTRXView(NavigationView &nav)                // Application Main 
+    APTRXView::APTRXView(NavigationView &nav)                // Application Main
     {
-     
         add_children({                                       // Add pointers for widgets
-            &button_helloWorld,
-            &label_progress,
-            &numberField_progress,
-            &progressBar_progress,
-            &timestamp,
+            &labels_gain,
+            &field_lna,
+            &field_vga,
+            &field_rf_amp,
+//            &rssi,
+            &labels_freq,
+            &options_freq,
+            &field_freq
         });
+//        progressBar_progress.set_max(PROGRESS_MAX);          // Set max for progress bar
+//        button_helloWorld.on_select = [this](Button &){      // Button logic
+//            if(progress < 100) {
+//                numberField_progress.set_value(100);         // Because numberField_progress has an on_change function,
+//            } else {                                         // progressBar_progress will update automatically.
+//                numberField_progress.set_value(0);
+//            }
+//        };
+//        numberField_progress.on_change = [this](int32_t v) { // When NumberField is changed
+//            progress = v;
+//            progressBar_progress.set_value(progress);
+//        };
+//        timestamp.set_seconds_enabled(true);                 // DateTime enable seconds
 
-        progressBar_progress.set_max(PROGRESS_MAX);          // Set max for progress bar
-
-        button_helloWorld.on_select = [this](Button &){      // Button logic
-            if(progress < 100) { 
-                numberField_progress.set_value(100);         // Because numberField_progress has an on_change function,
-            } else {                                         // progressBar_progress will update automatically.
-                numberField_progress.set_value(0);
+        options_freq.on_change = [this](size_t n, OptionsField::value_t v)
+        {
+            switch(n)
+            {
+                case 0: asdf=137620000; break;  // NOAA 15
+                case 1: asdf=137912500; break;  // NOAA 18
+                case 2: asdf=137100000; break;  // NOAA 19
             }
-        };                                                  
-
-        numberField_progress.on_change = [this](int32_t v) { // When NumberField is changed
-            progress = v;
-            progressBar_progress.set_value(progress);
+            field_freq.set_value(asdf);
         };
-
-        timestamp.set_seconds_enabled(true);                 // DateTime enable seconds
     }
 
     void APTRXView::update()                                 // Every time you get a DisplayFrameSync message this
